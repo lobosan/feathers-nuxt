c8d4de29690e39bcab1e0a4e5621c29ee11d6791
root <root@LP-W-BIENES.mcyp.local>
2017-04-12 12:21:43 -0500

    Host and port updated

--- src/app.hooks.js
+++ src/app.hooks.js
@@ -4,33 +4,33 @@
 const logger = require('./hooks/logger');
 
 module.exports = {
-  before: {
-    all: [],
-    find: [],
-    get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: []
-  },
+   before: {
+      all: [],
+      find: [],
+      get: [],
+      create: [],
+      update: [],
+      patch: [],
+      remove: []
+   },
 
-  after: {
-    all: [ logger() ],
-    find: [],
-    get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: []
-  },
+   after: {
+      all: [logger()],
+      find: [],
+      get: [],
+      create: [],
+      update: [],
+      patch: [],
+      remove: []
+   },
 
-  error: {
-    all: [ logger() ],
-    find: [],
-    get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: []
-  }
+   error: {
+      all: [logger()],
+      find: [],
+      get: [],
+      create: [],
+      update: [],
+      patch: [],
+      remove: []
+   }
 };
